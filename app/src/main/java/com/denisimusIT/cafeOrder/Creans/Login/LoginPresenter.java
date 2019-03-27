9cb029d3855db029801a705ca528ac609735c0a3
develop.qt@gmail.com <LenovoA706>
2019-03-27 21:32:23 +0200

    make LoginPresenter structure

---
+++ app/src/main/java/com/denisimusIT/cafeOrder/Creans/Login/LoginPresenter.java
@@ -0,0 +1,13 @@
+package com.denisimusIT.cafeOrder.Creans.Login;
+
+public class LoginPresenter implements LoginActyvityContract.LoginPresenter {
+    @Override
+    public void onButtonWasClicked() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+}
