1eb99e2d14c4ced41e390bc218f6d2b6a4b11947
develop.qt@gmail.com <LenovoA706>
2019-03-27 21:27:09 +0200

    Refactoring

--- app/src/main/java/com/denisimusIT/cafeOrder/Creans/Login/LoginActyvityContract.java
+++ app/src/main/java/com/denisimusIT/cafeOrder/Creans/Login/LoginActyvityContract.java
@@ -2,19 +2,24 @@ package com.denisimusIT.cafeOrder.Creans.Login;
 
 public interface LoginActyvityContract {
     interface LoginView {
-        void showButtonLoginText();
         void sendLogotapeImage();
+
+        void showButtonLoginText();
     }
 
     interface Presenter {
         void onButtonWasClicked();
+
         void onDestroy();
     }
 
     interface Model {
         void logotypeLoader();
+
         void loginValidator();
+
         void loadListOfGroup();
+
         void loadButtonLoginText();
 
 
